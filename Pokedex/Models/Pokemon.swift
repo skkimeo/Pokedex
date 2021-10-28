4b59c34b7cace731e5b2eeae5919300ab02d7091
sun <skkimeo@gmail.com>
2021-10-28 18:31:25 +0900

    created the Pokemon Model...

---
+++ Pokedex/Models/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by sun on 2021/10/28.
+//
+
+import Foundation
+
+
+// response
+struct Pokemons: Codable {
+    var pokemons: [Pokemon]
+}
+
+struct Pokemon: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let imgUrl: [[String: String]]
+//    let types: [String]
+//    let height: Int
+//    let weight: Int
+    
+    enum codingKeys: String, CodingKey {
+        case id, name
+        case imgUrl = "sprites"
+    }
+}
